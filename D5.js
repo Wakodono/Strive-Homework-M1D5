9eff5871efc8fa5a0f0f2365f7c81a3fae04d2e3
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-04 17:22:54 +0100

    Completed HW

--- D5.js
+++ D5.js
@@ -10,85 +10,103 @@ ASSIGNMENT RULES
 Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
 */
 
-const areaFunction = function(l1, l2) {
-     console.log(l1 * l2);
+function area(l1, l2) {
+     return l1 * l2;
 };
+console.log(area(12, 17))
 
 /* EXERCISE 2
-Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+Write a function "crazySum" which receives two integers. It should return the sum of those two values, 
+but if the two values are the same then it should return their sum multiplied by 3.
 */
 
-const crazySum = function(integer1, integer2){
-    return integer1 + integer2
+function crazySum (integer1, integer2){
+    if(integer1 === integer2){
+        return (integer1 + integer2)*3
+    }else{
+       return integer1 + integer2
+    }
 }
+
+console.log(crazySum(5, 4))
 
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
 It should return triple their absolute difference if the given number is greater than 19.
 */
 
-const crazyDiff = function(n, x){
-    let x = 19
-    return n - x
+function crazyDiff(n){
+if (n > 19) {
+    return Math.abs(n-19)*3
+} else {
+    return Math.abs(n-19)
 }
+}
+console.log(crazyDiff(12))
 
 /* EXERCISE 4
 Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
 */
-
-const boundary = function (n) {
-    if (n >= 20 && n <= 100 && n === 400) {
-        return true 
+function boundary(n) {
+    if ((n > 20 && n <= 100) || n === 400) {
+      return true;
+    } else {
+      return false;
     }
-}
+  }
+console.log(boundary(45))
 
-/* WRITE YOUR CODE HERE */
 
 /* EXERCISE 5
 Write a function "strivify" which accepts a string.
 It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-const strivify = function(s){
-console.log("Strive" + " " + s) // s stands for something. THe string that would be passed as a parameter to this function
-if (s == "'Strive' + s") {
-    return "Strive"
+function strivify(str){
+    if (str.startsWith("Strive")) {
+        return str;
+      } else {
+        return "Strive " + str;
+      }
 }
-}
+console.log(strivify("school"))
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
 
-//totally stuck on this one :')
-
-const check3and7 = function() {
-    
+function check3and7(n){
+    if (n >= 0 && n % 3 === 0 || n % 7 === 0) {
+        return true
+    } else {
+        return false
+    }
 }
 
+console.log(check3and7(6)
+)
 /* EXERCISE 7
 Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
 */
 
-const reverseString = function(str){
-    let newString = ""
-    for (i = str.length - 1; i >+ 0; i--){
-        newString += str[i]
-    }
-    return newString
+function reverseString(str){
+    let splitString = str.split("")
+    let reverseArray = splitString.reverse()
+    let joinArray = reverseArray.join("")
+    return joinArray
 }
-reverseString('Strive');
-
+console.log(reverseString("Supercalifragilisticexpialidocious"))
 
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
 */
 
-const upperFirst = function(word) {
-    let newWord = word.toUpperCase()
-console.log(newWord)
-}
+function upperFirst(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  console.log(upperFirst("centurion"));
+
 
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
@@ -96,14 +114,25 @@ Write a function "cutString" to create a new string without the first and last c
 
 //use splice to manipulate string to achieve this 
 
-/* WRITE YOUR CODE HERE */
+function cutString(str) {
+    return str.slice(1, str.length - 1);
+  }
+  console.log(cutString("sayonara"));
+
 
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
 */
 
-/* WRITE YOUR CODE HERE */
-
+function giveMeRandom(n) {
+    const arr = [];
+    for (let i = 0; i <= n; i++) {
+        const random = (Math.floor(Math.random() * 10))
+      arr.push(random);
+    }
+    return arr;
+  }
+  console.log(giveMeRandom(9));
 /* WHEN YOU ARE FINISHED
 Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
 */
